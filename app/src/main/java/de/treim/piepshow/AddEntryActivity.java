43399c9a787360d55a8a5757b1eea3fb8c86471a
Tobias Reim <reim.tobias@gmail.com>
2018-05-03 12:06:55 +0200

    adding image encoding

--- app/src/main/java/de/treim/piepshow/AddEntryActivity.java
+++ app/src/main/java/de/treim/piepshow/AddEntryActivity.java
@@ -1,16 +1,26 @@
 package de.treim.piepshow;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 public class AddEntryActivity extends AppCompatActivity {
-    private String path="";
+    private String b64image="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +36,48 @@ public class AddEntryActivity extends AppCompatActivity {
     }
 
     public void onCameraClick(View view){
-        //do nuffin
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, 427);
+        }
     }
+
     public void onSelectClick(View view){
-        //still nuffin
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, 428);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 427 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            b64image = encodeBmp(imageBitmap);
+        }
+        else if (requestCode == 428 && resultCode == RESULT_OK) {
+            Uri selectedImage = data.getData();
+            try {
+                // Do whatever you want with this bitmap (image)
+                Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                b64image=encodeBmp(bitmapImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String encodeBmp(Bitmap bmp){
+        ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOS);
+        return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //Send
-        if(path.equals("")){
+        if(b64image.equals("")){
             Toast.makeText(this,"Kein Bild ausgewählt",Toast.LENGTH_SHORT).show();
         }
         return true;
